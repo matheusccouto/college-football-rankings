48eec4d06abd0201df2cb45c15e47be3cb1b840f
Matheus Couto <matheusccouto@gmail.com>
2020-11-01 18:34:46 -0300

    Simplify app.py and apply OOP

--- college_football_rankings/__init__.py
+++ college_football_rankings/__init__.py
@@ -1,257 +1,318 @@
 """
-Estimate and predict college football rankings.
-
-Powered by collegefootballdata.com
+Estimate college football rankings.
 """
-from typing import Any, Dict, Sequence, Tuple, Optional, List, Callable
+import dataclasses
+from typing import Callable, Dict, List, Optional, Sequence
 
-import college_football_api as cf_api
-from college_football_rankings import iterative
-
-
-class NoGameError(Exception):
-    """ This team didn't play this week: """
+import cfbd
 
 
-def get_all_games_data(year: int) -> Sequence[Dict[str, Any]]:
-    """
-    Get all games data.
+@dataclasses.dataclass
+class Game:
+    """ Game data."""
 
-    Args:
-        year: Season year.
+    season: int
+    week: int
+    season_type: str
+    start_date: str
+    start_time_tbd: bool
+    neutral_site: bool
+    conference_game: bool
+    attendance: int
+    venue: str
+    team_points: int
+    team_line_scores: List[int]
+    team_post_win_prob: float
+    opponent: str
+    opponent_conference: str
+    opponent_points: int
+    opponent_line_scores: List[int]
+    opponent_post_win_prob: float
+    excitement_index: float
 
-    Returns:
-        Sequence of dictionaries with each game data.
-    """
-    api = cf_api.CollegeFootballAPI()
-    return api.games(year=year, season_type="regular")
-
-
-def get_all_teams_data(year: int) -> Sequence[Dict[str, Any]]:
-    """
-    Get all teams data.
-
-    Args:
-        year: Season year.
-
-    Returns:
-        Sequence of dictionaries with each FBS team data.
-    """
-    api = cf_api.CollegeFootballAPI()
-    return api.teams(year=year)
-
-
-def get_all_teams_names(year: int) -> Sequence[str]:
-    """
-    Get all teams names.
-
-    Args:
-        year: Season year.
-
-    Returns:
-        Sequence of FBS teams names.
-    """
-    return [data["school"] for data in get_all_teams_data(year)]
+    @property
+    def margin(self) -> Optional[int]:
+        """ Score margin. """
+        if self.team_points is None or self.opponent_points is None:
+            return None
+        return self.team_points - self.opponent_points
 
 
-def get_all_teams_logos(year: int) -> Dict[str, str]:
-    """
-    Get all teams logo.
+class Schedule:
+    """ Team's schedule. """
 
-    Args:
-        year: Season year.
+    def __init__(self, team: str):
+        self.team = team
+        self.games: List[Game] = []
 
-    Returns:
-        Sequence of FBS teams logo URL.
-    """
-    return {data["school"]: data["logos"][0] for data in get_all_teams_data(year)}
+    @property
+    def n_played_games(self):
+        """ Number of already played games this season. """
+        return len([game for game in self.games if game.team_points is not None])
+
+    @property
+    def n_games(self):
+        """ Number of games this season. """
+        return len(self.games)
+
+    def filter_score(self, max_week: int):
+        """ Clear schedule games score. """
+        for game in self.games:
+            if game.week > max_week:
+                game.team_points = None
+                game.team_line_scores = None
+                game.team_post_win_prob = None
+                game.opponent_points = None
+                game.opponent_line_scores = None
+                game.opponent_post_win_prob = None
+                game.attendance = None
+                game.excitement_index = None
+
+    def add_game(self, game: cfbd.Game):
+        """ Add games to schedule. """
+        if game.home_team == self.team:
+            team_points = game.home_points
+            team_line_scores = game.home_line_scores
+            team_post_win_prob = game.home_post_win_prob
+            opponent = game.away_team
+            opponent_conference = game.away_conference
+            opponent_points = game.away_points
+            opponent_line_scores = game.away_line_scores
+            opponent_post_win_prob = game.away_post_win_prob
+        else:
+            team_points = game.away_points
+            team_line_scores = game.away_line_scores
+            team_post_win_prob = game.away_post_win_prob
+            opponent = game.home_team
+            opponent_conference = game.home_conference
+            opponent_points = game.home_points
+            opponent_line_scores = game.home_line_scores
+            opponent_post_win_prob = game.home_post_win_prob
+
+        game_pov = Game(
+            season=game.season,
+            week=game.week,
+            season_type=game.season_type,
+            start_date=game.start_date,
+            start_time_tbd=game.start_time_tbd,
+            neutral_site=game.neutral_site,
+            conference_game=game.conference_game,
+            attendance=game.attendance,
+            venue=game.venue,
+            team_points=team_points,
+            team_line_scores=team_line_scores,
+            team_post_win_prob=team_post_win_prob,
+            opponent=opponent,
+            opponent_conference=opponent_conference,
+            opponent_points=opponent_points,
+            opponent_line_scores=opponent_line_scores,
+            opponent_post_win_prob=opponent_post_win_prob,
+            excitement_index=game.excitement_index,
+        )
+        self.games.append(game_pov)
 
 
-def create_teams_schedule_dict(
-    games: Sequence[Dict[str, Any]]
-) -> Dict[str, Dict[int, Tuple[Optional[str], Optional[Tuple[int, int]]]]]:
-    """
-    Create teams schedules dictionary.
+class Team:
+    """ Team object. """
 
-    Args:
-        games: Games data.
+    def __init__(
+        self,
+        name: str,
+        logos: Optional[List[str]] = None,
+        alt_name: Optional[str] = None,
+    ):
+        self.name = name
+        self.alt_name = alt_name
+        self.power = 0
+        self.schedule = Schedule(name)
 
-    Returns:
-        Teams schedules dictionary.
-    """
-    teams_dict: Dict[str, Dict[int, Tuple[Optional[str], Optional[int]]]] = {}
-    last_week = 0  # Measure the last week from season.
+        if logos:
+            self.logo_light = logos[0]
+            self.logo_dark = logos[1]
+        else:
+            self.logo_light = None
+            self.logo_dark = None
+
+    def __repr__(self) -> str:
+        return self.name
+
+    @property
+    def wins(self) -> int:
+        """ Get number of wins."""
+        return len(
+            [
+                game
+                for game in self.schedule.games
+                if game.margin is not None and game.margin > 0
+            ]
+        )
+
+    @property
+    def draws(self) -> int:
+        """ Get number of draws."""
+        return len(
+            [
+                game
+                for game in self.schedule.games
+                if game.margin is not None and game.margin == 0
+            ]
+        )
+
+    @property
+    def losses(self) -> int:
+        """ Get number of losses."""
+        return len(
+            [
+                game
+                for game in self.schedule.games
+                if game.margin is not None and game.margin < 0
+            ]
+        )
+
+    @property
+    def record(self) -> str:
+        """ Get team record. """
+        wins = str(self.wins)
+        draws = str(self.draws)
+        losses = str(self.losses)
+        if draws:
+            "-".join([wins, draws, losses])
+        return "-".join([wins, losses])
+
+
+class Ranking:
+    """ Yearly rankings. """
+
+    def __init__(self, name: str):
+        self.name = name
+        self._rankings: Dict[int, List[str]] = {}
+
+    def add_week(self, week: int, rankings: Sequence[str]):
+        """ Add weekly rankings. """
+        self._rankings[week] = list(rankings)
+
+    def get_week(self, week: int) -> List[str]:
+        """ Get rankings from an specified week. """
+        if week not in self._rankings:
+            return []
+        return self._rankings[week]
+
+    def team_position(self, team: str, week: int) -> Optional[int]:
+        """ Get team position in a specific week. """
+        if week not in self._rankings:
+            return None
+        if team not in self._rankings[week]:
+            return None
+        return self._rankings[week].index(team) + 1
+
+    def team_history(self, team: str) -> List[Optional[int]]:
+        """ Team ranking history. """
+        return [self.team_position(team, week) for week in self._rankings]
+
+
+def get_games(*args, **kwargs) -> List[cfbd.Game]:
+    """ Get games data. """
+    config = cfbd.Configuration()
+    config.verify_ssl = False
+    api = cfbd.GamesApi(cfbd.ApiClient(config))
+    return api.get_games(*args, **kwargs)
+
+
+def get_teams(**kwargs) -> List[cfbd.Team]:
+    """ Get teams data. """
+    config = cfbd.Configuration()
+    config.verify_ssl = False
+    api = cfbd.TeamsApi(cfbd.ApiClient(config))
+    return api.get_teams(**kwargs)
+
+
+def get_rankings(*args, **kwargs) -> List[cfbd.RankingWeek]:
+    """ Get rankings data. """
+    config = cfbd.Configuration()
+    config.verify_ssl = False
+    api = cfbd.RankingsApi(cfbd.ApiClient(config))
+    return api.get_rankings(*args, **kwargs)
+
+
+def create_teams_instances(teams: List[cfbd.Team]) -> Dict[str, Team]:
+    """ Create FPS teams instance. """
+    return {team.school: Team(team.school, team.logos) for team in teams}
+
+
+def clean_scores(teams: Dict[str, Team], max_week: int):
+    """ Clear all teams schedules. """
+    for team in teams.values():
+        team.schedule.filter_score(max_week)
+
+
+def fill_schedules(
+    games: List[cfbd.Game], teams: Dict[str, Team],
+):
+    """ Fill all teams schedules with games. """
     for game in games:
-
-        week = game["week"]
-        home_team = game["home_team"]
-        away_team = game["away_team"]
-
-        # Update last_week.
-        if week > last_week:
-            last_week = week
-
-        # If points is None. It means the game didn't happened yet, or was cancelled.
-        if game["home_points"] is not None:
-            home_score = (game["home_points"], game["away_points"])
-            away_score = (game["away_points"], game["home_points"])
-        else:
-            home_score = None
-            away_score = None
-
-        # Update dictionary if there is already an entry for this team.
-        if home_team in teams_dict:
-            teams_dict[home_team].update({week: (away_team, home_score)})
-        else:
-            teams_dict[home_team] = {week: (away_team, home_score)}
-
-        # Do the same for away team.
-        if away_team in teams_dict:
-            teams_dict[away_team].update({week: (home_team, away_score)})
-        else:
-            teams_dict[away_team] = {week: (home_team, away_score)}
-
-    for schedules in teams_dict.values():
-        for i in range(1, last_week + 1):
-            if i not in schedules:
-                schedules[i] = (None, None)
-
-    return teams_dict
+        teams[game.home_team].schedule.add_game(game)
+        teams[game.away_team].schedule.add_game(game)
 
 
-def create_teams_margins_dict(
-    teams_schedules: Dict[
-        str, Dict[int, Tuple[Optional[str], Optional[Tuple[int, int]]]]
-    ]
-) -> Dict[str, Dict[int, Tuple[Optional[str], Optional[int]]]]:
-    """
-    Create teams margins dictionary.
-
-    Args:
-        teams_schedules: Teams schedule data.
-
-    Returns:
-        Teams margins dictionary.
-    """
-    teams_dict: Dict[str, Dict[int, Tuple[Optional[str], Optional[int]]]] = {}
-    # Iterate through all teams.
-    for team, schedules in teams_schedules.items():
-        margins: Dict[int, Tuple[Optional[str], Optional[int]]] = {}
-        # Calculates margin.
-        for week, data in schedules.items():
-            rival, score = data  # Unpack
-            if score is not None:
-                margins[week] = (rival, score[0] - score[-1])
-            else:
-                margins[week] = data
-        teams_dict[team] = margins
-
-    return teams_dict
+def filter_schedules(teams: Dict[str, Team], max_week: int):
+    """ Filter data from played games up to a specified wek.. """
+    for team in teams.values():
+        team.schedule.filter_score(max_week)
 
 
-def project_teams_margins(
-    teams_margins: Dict[str, Dict[int, Tuple[Optional[str], Optional[int]]]],
-    teams_rankings: Sequence[str],
-    max_week: int
-):
-    """
-    Include projections in teams margins dictionary.
-
-    Change inplace.
-
-    Args:
-        teams_margins: Teams margins dictionary.
-        teams_rankings: Teams rankings standings.
-        max_week: Max week to project.
-
-    Returns:
-        Teams margins dictionary.
-    """
-    max_points = 50
-    for team, schedule in teams_margins.items():
-        # Get ranking and invert to calculate power.
-        try:
-            team_ranking = teams_rankings.index(team)
-        except ValueError:
-            team_ranking = len(teams_rankings)
-        team_power = 1 - (team_ranking / len(teams_rankings))
-        for week, game in schedule.items():
-            if week > max_week:
-                break
-            rival, margin = game
-            # Look for blank games to project.
-            if rival is not None and margin is None:
-                try:
-                    rival_ranking = teams_rankings.index(rival)
-                except ValueError:
-                    rival_ranking = len(teams_rankings)
-                rival_power = 1 - (rival_ranking / len(teams_rankings))
-                projected_margin = int((team_power - rival_power) * max_points)
-                teams_margins[team][week] = (rival, projected_margin)
+def clean_teams(teams: Dict[str, Team]) -> Dict[str, Team]:
+    """ Remove entries that didn't play a single game that season. """
+    return {name: team for name, team in teams.items() if team.schedule.n_games > 0}
 
 
-def filter_week(
-    teams_margins: Dict[str, Dict[int, Tuple[Optional[str], Optional[int]]]], week: int,
-):
-    """
-    Erase data from every week after the one specified.
-
-    Args:
-        teams_margins: Each team's margins.
-        week: Max week to keep.
-
-    Returns:
-        Margins with weeks above the limit erased.
-    """
-    for margins in teams_margins.values():
-        for game_week in margins.keys():
-            if game_week > week:
-                rival = margins[game_week][0]
-                margins[game_week] = (rival, None)
+def last_played_week(games: List[cfbd.Game]) -> int:
+    """ Get last played week. """
+    for game in reversed(games):
+        if game.home_points is not None:
+            return game.week
+    return 0
 
 
-def evaluate(
-    teams_margins: Dict[str, Dict[int, Tuple[Optional[str], Optional[int]]]],
-    func: Callable,
-    *args,
-    **kwargs,
-) -> List[str]:
-    """
-    Evaluate rankings.
+def create_polls(year: int, max_week: Optional[int] = None) -> Dict[str, Ranking]:
+    """ Create polls instances. """
+    week_list = get_rankings(year=year)
 
-    Args:
-        teams_margins: Teams margins.
-        func: Function to evaluate teams power.
-        args: Positional argument from func.
-        kwargs: Keyword arguments from func.
+    # Create instances.
+    instances = {
+        "Playoff Committee Rankings": Ranking(name="Playoff Committee Rankings"),
+        "AP Top 25": Ranking(name="AP Top 25"),
+        "Coaches Poll": Ranking(name="Coaches Poll"),
+    }
 
-    Returns:
-        Teams rankings.
-    """
-    # Run power function;
-    teams_power = func(teams_margins, *args, **kwargs)
+    for week in week_list:
 
-    # Unpack and sort teams.
-    teams = list(teams_power.keys())
-    power = list(teams_power.values())
-    return [team for _, team in sorted(zip(power, teams), reverse=True)]
+        week_number = week_list.index(week) + 1
+        if max_week:
+            if week_number > max_week:
+                return instances
+
+        for poll in week.polls:
+            if poll.poll in instances:
+                ranks = [rank.school for rank in poll.ranks]
+                instances[poll.poll].add_week(week_number, ranks)
+
+    return instances
 
 
-def create_records_dict(
-    teams_margins: Dict[str, Dict[int, Tuple[Optional[str], Optional[int]]]]
-) -> Dict[str, str]:
-    """
-    Create dictionary with each team's aggregate records.
+class RankingError(Exception):
+    """ Exception that indicates that something prevented ranking calculation. """
 
-    Args:
-        teams_margins: Each team's margins for every game.
 
-    Returns:
-        Dictionary with team as key and aggregated records as vale.
-    """
-    records_dict: Dict[str, str] = {}
-    for name, margins in teams_margins.items():
-        n_wins = sum([1 for game in margins.values() if game[-1] and game[-1] > 0])
-        n_loses = sum([1 for game in margins.values() if game[-1] and game[-1] < 0])
-        records_dict.update({name: f"{n_wins}-{n_loses}"})
-    return records_dict
+def evaluate(teams: Dict[str, Team], func: Callable, *args, **kwargs) -> List[Team]:
+    """ Evaluate rankings. """
+    func(teams, *args, **kwargs)
+    return list(sorted(teams.values(), key=lambda obj: obj.power, reverse=True))
+
+
+def set_week_and_eval(
+    week: int, teams: Dict[str, Team], func: Callable, *args, **kwargs
+) -> List[Team]:
+    """ Set games for an specific week and evaluate. """
+    filter_schedules(teams=teams, max_week=week)
+    # teams = clean_teams(teams=teams)
+    return evaluate(teams, func, *args, **kwargs)
---
+++ ui/__init__.py
@@ -0,0 +1,37 @@
+""" User interface. """
+
+import datetime
+from typing import Dict, Sequence
+
+import streamlit as st
+
+import college_football_rankings as cfr
+
+
+def year_selector(first_year: int):
+    """ Year selector widget. """
+    this_year = datetime.date.today().year
+    return st.selectbox("Select year", options=range(this_year, first_year - 1, -1))
+
+
+def week_selector(n_weeks: int):
+    """ Week selector widget. """
+    return st.slider("Select week", min_value=1, max_value=n_weeks, value=n_weeks)
+
+
+def format_html_table(html):
+    """ Format a html table. """
+    html = html.replace("<table", "<table width=100%")
+    html = html.replace("<table", '<table style="text-align: right;"')
+    return html
+
+
+def create_html_tag(team: cfr.Team):
+    """ Create html tag with logo, team name and record. """
+    return f'<img src="{team.logo_light}" height="16"> {team.name} {team.record}'
+
+
+def add_logos_and_records(names: Sequence, teams: Dict[str, cfr.Team]) -> Sequence:
+    """ Add teams logo and records to every team name. """
+    values = [create_html_tag(teams[team]) for team in names]
+    return values
